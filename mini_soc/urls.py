0b561b5bbe27c4740bfb7e0723500fe3a4858d01
hari <harinand084@gmail.com>
2026-04-05 19:17:59 +0530

    Fixed static files

--- mini_soc/urls.py
+++ mini_soc/urls.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from logs import views as logs_views
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_admin(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@gmail.com',
+            password='admin123'
+        )
+        return HttpResponse("Admin created")
+    return HttpResponse("Admin already exists")
 
 urlpatterns = [
+    path('create-admin/', create_admin),
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('logs/', include('logs.urls')),
